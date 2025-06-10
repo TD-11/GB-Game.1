dbecd93004816cf0a2990fa7bbc65f31a86199f9
TD-11 <tulio.s@escolar.ifrn.edu.br>
2025-06-10 14:51:05 -0300

    Retoques

--- Jogo.1/Assets/Scripts/Player.cs
+++ Jogo.1/Assets/Scripts/Player.cs
@@ -4,14 +4,26 @@ public class Player : MonoBehaviour
 {
     public float maxSpeed;
     public float speed;
+    public float hitForce;
+    private int vidas = 3;
     private Rigidbody2D rb;
-    private Vector2 moviment1 = new Vector2(0.5f,1f);
-    private Vector2 moviment2 = new Vector2(-0.5f,1);
-    private Vector2 moviment3 = new Vector2(0,1);
-    private Vector2 moviment4 = Vector2.zero; 
+    private Vector2 movement1 = new Vector2(0.5f,1f);
+    private Vector2 movement2 = new Vector2(-0.5f,1);
+    private Vector2 movement3 = new Vector2(0,1);
+    private Vector2 movement4 = Vector2.zero; 
+    private Vector2 hit = new Vector2(0,-1);
+    
+    private float initialRotationZ;
+    private float holdTimer = 0f;
+    public float holdTimeToStartRotating = 1.0f; // Tempo segurando A para começar a rotacionar
+    public float maxRotationAngle = 45f;         // Rotação máxima permitida para a direita (em graus)
+    public float rotationSpeed = 30f;            // Velocidade de rotação em graus por segundo
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        initialRotationZ = transform.eulerAngles.z;
     }
 
     void Update()
@@ -21,29 +33,33 @@ public class Player : MonoBehaviour
         
         if (right && left)
         {
-            rb.AddForce(moviment3 * speed,ForceMode2D.Force);
+            rb.AddForce(movement3 * speed,ForceMode2D.Force);
         }
         else if (left)
         {
-            rb.AddForce(moviment1 * speed,ForceMode2D.Force);
+            rb.AddForce(movement1 * speed,ForceMode2D.Force);
         }
         else if (right)
         {
-            rb.AddForce(moviment2 * speed, ForceMode2D.Force);
+            rb.AddForce(movement2 * speed, ForceMode2D.Force);
         }
         else
         {
-            rb.AddForce(moviment4,0);
+            rb.AddForce(movement4,0);
         }
-        
-        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpeed);
 
+        rb.linearVelocity = Vector2.ClampMagnitude(rb.linearVelocity, maxSpeed);
     }
-    
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Danger")
+        {
+            rb.linearVelocity = hit * hitForce;
+            vidas--;
+        }
+
+        if (vidas == 0)
         {
             Destroy(gameObject);
         }
